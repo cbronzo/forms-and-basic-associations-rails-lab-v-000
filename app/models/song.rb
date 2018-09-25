d713208a94ca47bc90bb4d26d302ffc2e8df48b1
Catherine Bronzo <cbronzo@mac.com>
2018-09-25 21:21:11 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,10 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
   
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+  
   
   
   
